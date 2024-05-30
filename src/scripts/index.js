4a6260658b32ae44810e3e8c79ee4b7495548f0b
Tim Nicholas <tsnicholas@outlook.com>
2024-05-30 11:07:56 -0400

    Refactor webpack to be more reliable

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,6 @@
 import metadata from "./data/metadata.json";
+import "../styles.css"
+import "../assets/Professional Mugshot.jpg"
 
 const DATA_CONTAINER_CLASSES = "d-flex justify-content-between align-items-start";
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,52 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
     mode: "development",
-    entry: "./scripts/index.js",
+    entry: "./src/scripts/index.js",
     output: {
-        filename: "script.js",
+        filename: "bundle.js",
         path: path.resolve(__dirname, "dist"),
     },
     devServer: {
         port: 9000,
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.json$/,
+                type: 'json'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+                use: 'file-loader'
+            }
+        ]
+    },
     resolve: {
+        extensions: ['.js', '.json'],
         fallback: {
             "location": false,
             "navigator": false,
             "xmlhttprequest": false,
             "jsdom": false
         }
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/index.html",
+            filename: "index.html"
+        })
+    ]
 }
